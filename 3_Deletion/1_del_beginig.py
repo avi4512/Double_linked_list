c92233c9d15947dd6cfbbdc3d1fce9522146855d
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-06-27 11:44:14 +0530

    Create 1_del_beginig.py

---
+++ 3_Deletion/1_del_beginig.py
@@ -0,0 +1,77 @@
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class Double_Linked_list:
+
+    def __init__(self):
+        self.head = None
+
+    def display(self):
+        if self.head is None:
+            print("Linked List is Empty...!")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data,'-->',end=" ")
+                temp = temp.next
+    def backward(self):
+        if self.head is None:
+            print("Linked List is Empty...!")
+
+        #to find the last node in the linked list
+        else:
+            temp = self.head
+            while temp.next:
+                temp = temp.next
+
+            #to print data from the last node
+            while temp:
+                print(temp.data,'-->',end=" ")
+                temp = temp.prev
+
+    def del_begin(self):
+        if self.head is None:
+            print("Can't remove node from Empty linked list..")
+            return
+        if self.head.next is None:
+            self.head = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+
+
+
+
+
+
+
+
+dl = Double_Linked_list()
+
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+n4 = Node(40)
+
+dl.head = n1
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+#prev
+n2.prev = n1
+n3.prev = n2
+n4.prev = n3
+
+dl.del_begin()
+
+
+dl.display()
+print()
+dl.backward()
+
+
