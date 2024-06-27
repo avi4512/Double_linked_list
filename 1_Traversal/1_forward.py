d7f1f4fdf3b6fe48b5980ce8d259c7caffed47f4
avi4512 <144973797+avi4512@users.noreply.github.com>
2024-06-27 11:38:59 +0530

    Create 1_forward.py

---
+++ 1_Traversal/1_forward.py
@@ -0,0 +1,40 @@
+class Node:
+
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class Double_Linked_list:
+
+    def __init__(self):
+        self.head = None
+
+    def display(self):
+        if self.head is None:
+            print("Linked List is Empty...!")
+        else:
+            temp = self.head
+            while temp:
+                print(temp.data,'-->',end=" ")
+                temp = temp.next
+dl = Double_Linked_list()
+
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+n4 = Node(40)
+
+dl.head = n1
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+#prev
+n2.prev = n1
+n3.prev = n2
+n4.prev = n3
+
+dl.display()
+
+
